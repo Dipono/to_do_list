215e7f47847fcb2cd5aa3bcc434d6f1eb11991a0
dipono <joel.buti97@gmail.com>
2022-06-22 09:36:54 +0200

    added react icon for cancel

--- src/Components/AddnEdit/Add.jsx
+++ src/Components/AddnEdit/Add.jsx
@@ -1,11 +1,13 @@
 import './Add.css';
+import {AiFillCloseCircle,} from 'react-icons/ai';
+
 
 function Add(props) {
     return (props.trigger) ? (
 
         <div className="popup">
             <div className="popup-inner">
-                <button className="popup-btn" onClick={() => props.setTrigger(false)}>close</button>
+                <button className="popup-btn" onClick={() => props.setTrigger(false)}>< AiFillCloseCircle /></button>
                 {props.children}
             </div>
         </div>
--- src/Components/AddnEdit/Edit.jsx
+++ src/Components/AddnEdit/Edit.jsx
@@ -1,11 +1,12 @@
 import './Add.css';
+import {AiFillCloseCircle,} from 'react-icons/ai';
 
 function Edit(props) {
     return (props.triggerEdit) ? (
 
         <div className="popup">
             <div className="popup-inner">
-                <button className="popup-btn" onClick={() => props.setTriggerEdit(false)}>close</button>
+                <button className="popup-btn" onClick={() => props.setTriggerEdit(false)}>< AiFillCloseCircle /></button>
                 {props.children}
             </div>
         </div>
